9613f77788ecc5ab87a99b6e8461cf26d46ff574
PATRICK MEDIODIA <pvmediodia@live.mcl.edu.ph>
2019-05-03 11:57:09 +0800

    initial commit

---
+++ PODBProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PODBProject.Startup))]
+namespace PODBProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
